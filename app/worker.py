5a12f6ee27a4a9ef4caa16da1312977e2dcd5803
aylivdele <irassomahin@gmail.com>
2026-02-22 13:35:24 +0500

    set log level

--- app/worker.py
+++ app/worker.py
@@ -1,8 +1,10 @@
 import asyncio
+import logging
 from app.app_factory import AppFactory
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
     factory = AppFactory()
     scheduler = factory.create_scheduler()
     await scheduler.start()
